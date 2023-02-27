48eceed0d2426881d5b9e08d13a9146e6560d7e2
jaganjavid <jagantutor1@gmail.com>
2023-02-27 21:09:42 +0530

    js starting

---
+++ javascript/1-variable.js
@@ -0,0 +1,26 @@
+// A JavaScript variable is simply a name of storage location. 
+
+// var, let , const
+
+var name = "Jagan";
+name = "Javid";
+
+// Intialize Variable
+var greeting;
+
+greeting = "hello";
+
+// letters, _ , $
+
+// Can not start with number
+// var 5 = 5;
+
+// Multi words vars
+
+var firstname = "Jagan"; // normal case 
+var firstName = "camel case"; // ***
+var first_name = "Underscore" 
+var FirstName = "Pascal Case";
+var $firstName = "Jagan"; 
+
+console.log($firstName);
