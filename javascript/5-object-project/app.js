6d7f3313c97891e613014b72269f829ffae20eb9
jaganjavid <jagantutor1@gmail.com>
2023-04-27 19:32:48 +0530

    object es5 done

--- javascript/5-object-project/app.js
+++ javascript/5-object-project/app.js
@@ -7,8 +7,8 @@ function Book(title, author, isbn){
 }
 
 function UI(){
-    // Add Book to list
 
+    // Add Book to list
     UI.prototype.addBooktoList = function(book){
         const list = document.querySelector("#book-list");
 
@@ -23,13 +23,49 @@ function UI(){
             <td>${book.isbn}</td>
             <td>
                 <button type="button"
-                class="btn btn-danger">Delete</button>
+                class="btn btn-danger delete">Delete</button>
             </td>
         `;
 
         list.appendChild(row);
 
     }
+
+    // Show Alert
+
+    UI.prototype.showAlert = function(message, className){
+        // Create Div
+        const div = document.createElement("div");
+
+        // Add Class
+        div.className = `alert ${className}`;
+
+        // Add Text 
+        div.appendChild(document.createTextNode(message));
+
+        // Get show-alert
+        const showAlert = document.querySelector(".show-alert");
+
+        // InserAlert
+        showAlert.appendChild(div);
+
+        // SetTime out
+        setTimeout(function(){
+           document.querySelector(".alert").remove();
+        },3000)
+    }
+
+    // Delete Fields
+    UI.prototype.deleteBook = function(target){
+        target.remove();
+    }
+
+    // Clear Fields
+    UI.prototype.clearFileds = function(){
+        document.querySelector("#title").value = "";
+        document.querySelector("#author").value = "";
+        document.querySelector("#isbn").value = "";
+    } 
 }
 
 
@@ -48,10 +84,20 @@ document.getElementById("book-form")
     const ui = new UI();
 
     if(title === "" || author === "" || isbn === ""){
-        alert("please fill all the fileds")
+        ui.showAlert("please fill all the fileds", "alert-danger");
         return;
     } else{
         ui.addBooktoList(book);
+        ui.showAlert("Book Added Succesfully", "alert-success");
+        ui.clearFileds();
     }
+})
+
+
+document.getElementById("book-list").addEventListener("click", function(e){
+    e.preventDefault();
+    const ui = new UI();
+    ui.deleteBook(e.target.parentElement.parentElement);
+    ui.showAlert("Book Deleted Succesfully", "alert-success");
 })
 
