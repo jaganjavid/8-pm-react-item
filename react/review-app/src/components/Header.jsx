69e7520a6696aaddcd8fc215dcb611b1bd9edb94
jaganjavid <jagantutor1@gmail.com>
2023-06-15 19:50:42 +0530

    created first component

---
+++ react/review-app/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+
+const Header = ({text}) => {
+
+  return (
+    <header>
+        <div className="container">
+            <h2>{text}</h2>
+        </div> 
+    </header>
+  )
+}
+
+
+Header.defaultProps = {
+    text: "Review App"
+}
+
+Header.propTypes = {
+    text: PropTypes.string
+}
+
+export default Header
+
+
+
+// RAFCE
