a55220822d0a4fd048dc3929df3cbcfe81d1d499
jaganjavid <jagantutor1@gmail.com>
2023-06-20 20:00:55 +0530

    added second components

--- react/review-app/src/App.js
+++ react/review-app/src/App.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
+
+  const [feedback, setFeedback] = useState([
+    {
+      id:1,
+      rating:1,
+      text:"This is smaple text 1"
+    },
+    {
+      id:2,
+      rating:5,
+      text:"This is smaple text 2"
+    },
+    {
+      id:3,
+      rating:8,
+      text:"This is smaple text 3"
+    },
+  ]);
 
   return (
    <div>
       <Header/>
+      <div className="container">
+          <FeedbackList feedback={feedback}/>
+      </div>
    </div>
   )
 }
--- react/review-app/src/components/Header.jsx
+++ react/review-app/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
 
 
-const Header = ({text}) => {
+const Header = ({text, bgColor, textColor}) => {
+
+  const headerStyle = {
+    backgroundColor:bgColor,
+    color:textColor
+  }
 
   return (
-    <header>
+    <header style={headerStyle}>
         <div className="container">
             <h2>{text}</h2>
         </div> 
@@ -14,11 +19,15 @@ const Header = ({text}) => {
 
 
 Header.defaultProps = {
-    text: "Review App"
+    text: "Review App",
+    bgColor:"rgba(0,0,0,0.5)",
+    textColor: "#ffffff"
 }
 
 Header.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
 }
 
 export default Header
